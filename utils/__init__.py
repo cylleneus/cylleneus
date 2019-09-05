1f921271721076912daf72398d425b1ead560d8c
William Michael Short <w.short@exeter.ac.uk>
2019-09-05 10:46:41 +0100

    clean up dependencies

--- utils/__init__.py
+++ utils/__init__.py
@@ -101,29 +101,3 @@ def nrange(start, end):
 
     for se in range(_toint(start, base), _toint(end, base) + 1):
         yield _totuple(se, base, len(start))
-
-
-def roman_to_arabic(n: str):
-    """ Convert a Roman numeral to an Arabic integer. """
-
-    if isinstance(n, str):
-        n = n.upper()
-        numerals = {'M': 1000,
-                    'D': 500,
-                    'C': 100,
-                    'L': 50,
-                    'X': 10,
-                    'V': 5,
-                    'I': 1
-                    }
-        sum = 0
-        for i in range(len(n)):
-            try:
-                value = numerals[n[i]]
-                if i+1 < len(n) and numerals[n[i+1]] > value:
-                    sum -= value
-                else:
-                    sum += value
-            except KeyError:
-                return None
-        return sum
