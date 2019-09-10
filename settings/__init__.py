65ae4554d3c752511f3ef37968a131b7f8b960fe
William Short <wmshort@outlook.com>
2019-09-10 11:32:15 +0100

    bump version number

--- settings/__init__.py
+++ settings/__init__.py
@@ -5,7 +5,7 @@ import sys
 from utils import DEBUG_HIGH, DEBUG_MEDIUM, DEBUG_LOW, DEBUG_OFF
 
 # Version settings
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 # Platform
 PLATFORM = sys.platform
