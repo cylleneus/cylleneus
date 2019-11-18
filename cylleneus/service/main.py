cd3afd4fe09154ac8f6f240327a3c66bed01d788
ws308 <w.short@exeter.ac.uk>
2019-11-18 19:32:30 +0000

    update FastAPI search service

--- cylleneus/service/main.py
+++ cylleneus/service/main.py
@@ -1,8 +1,8 @@
 import os
-from typing import List, Tuple
+from typing import List
 
 from celery.result import AsyncResult
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
@@ -11,46 +11,51 @@ from starlette.status import HTTP_200_OK, HTTP_202_ACCEPTED, HTTP_204_NO_CONTENT
 from . import tasks
 
 
-class Query(BaseModel):
+class CylleneusWork(BaseModel):
+    corpus: str
+    docix: int
+
+
+class CylleneusQuery(BaseModel):
     query: str
-    collection: List[Tuple[str, int]] = None
+    collection: List[CylleneusWork] = None
 
 
 # Adjust as appropriate
 root_dir = os.path.expanduser('')
-load_dotenv(os.path.join(root_dir, '.env'))
+#load_dotenv(os.path.join(root_dir, '.env'))
 
 app = FastAPI()
 
 
-@app.post("/query/")
-async def query(query: Query):
-    q = query["query"]
-    c = query["collection"]
-    result = tasks.search.delay(q, c)
-    return JSONResponse(status_code=HTTP_202_ACCEPTED, content={
+@app.post("/search/", status_code=HTTP_202_ACCEPTED)
+async def search(query: CylleneusQuery):
+    result = tasks.search.delay(query.query, query.collection)
+    return JSONResponse(content={
         'id': result.id,
     })
 
 
-@app.get("/status/{id}")
-async def status(id):
-    result = AsyncResult(id=id, backend='redis://localhost')
+@app.get("/status/")
+async def status(id: str):
+    result = tasks.search.AsyncResult(id)
 
     return {"status": result.status}
 
 
-@app.get("/results/{id}")
-async def results(id):
-    result = AsyncResult(id=id, backend='redis://localhost')
+@app.get("/results/", status_code=HTTP_200_OK)
+async def results(id: str):
+    result = tasks.search.AsyncResult(id)
 
     if result.ready():
-        if result.result:
-            response = JSONResponse(status_code=HTTP_200_OK, content={
-                "result": result.result
+        response = JSONResponse(content={
+                "result": result.get()
             })
-        else:
-            response = JSONResponse(status_code=HTTP_204_NO_CONTENT)
     else:
         response = JSONResponse(status_code=HTTP_400_BAD_REQUEST)
     return response
+
+@app.get("/index/", status_code=HTTP_200_OK)
+async def index(corpus: str):
+    result = tasks.index(corpus)    
+    return JSONResponse(content={ corpus: result })
--- cylleneus/service/tasks.py
+++ cylleneus/service/tasks.py
@@ -9,17 +9,20 @@ Cylleneus = Celery(
     backend='redis://localhost',
     broker='pyamqp://guest@localhost//',
 )
-Cylleneus.conf.broker_heartbeat = 0
-Cylleneus.conf.result_persistent = True
-Cylleneus.conf.worker_prefetch_multiplier = 0
 
 
-@Cylleneus.task(acks_late=True)
+@Cylleneus.task
 def search(q, collection=None):
     if collection:
-        c = Collection([Corpus(corpus).work_by_docix(docix) for corpus, docix in collection])
+        c = Collection([Corpus(work.corpus).work_by_docix(work.docix) for work in collection])
     else:
-        c = Collection(Corpus("lasla").works)
+        c = Collection(Corpus("perseus").works)
     searcher = Searcher(c)
     s = searcher.search(q)
     return s.to_json()
+
+@Cylleneus.task
+def index(corpus):
+    return [ { "docix": work.docix,
+          "author": work.author,
+          "title": work.title } for work in Corpus(corpus).works]
